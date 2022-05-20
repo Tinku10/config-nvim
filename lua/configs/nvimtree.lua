bdc95d6fbd365583f127dba532c34c14736dc714
Tinku Kalita <tinku.kvs@gmail.com>
2022-05-20 08:05:11 +0530

    Migrate options in NvimTree

--- lua/configs/nvimtree.lua
+++ lua/configs/nvimtree.lua
@@ -2,7 +2,6 @@ vim.cmd [[ packadd nvim-tree.lua ]]
 
 vim.g.nvim_tree_auto_ignore_ft = { 'startify', 'dashboard' } --"empty by default, don't auto open tree on specific filetypes.
 -- vim.g.nvim_tree_quit_on_open = 1 --"0 by default, closes the tree when you open a file
-vim.g.nvim_tree_indent_markers = 1 --"0 by default, this option shows indent markers when folders are open
 vim.g.nvim_tree_git_hl = 1 --"0 by default, will enable file highlight for git attributes (can be used without the icons).
 vim.g.nvim_tree_root_folder_modifier = ':~' --"This is the default. See :help filename-modifiers for more options
 vim.g.nvim_tree_width_allow_resize  = 1 --"0 by default, will not resize the tree when opening a file
@@ -74,11 +73,27 @@ require'nvim-tree'.setup {
   hijack_netrw        = true,
   open_on_setup       = false,
   ignore_ft_on_setup  = {},
-  auto_close          = false,
   open_on_tab         = false,
   hijack_cursor       = false,
   update_cwd          = false,
   update_to_buf_dir   = {
+    enable = true,
+    auto_open = true,
+  },
+  renderer = {
+    indent_markers = {
+      enable = false,
+      icons = {
+        corner = "└ ",
+        edge = "│ ",
+        none = "  ",
+      },
+    },
+    icons = {
+      webdev_colors = true,
+    },
+  },
+  hijack_directories = {
     enable = true,
     auto_open = true,
   },
@@ -102,7 +117,8 @@ require'nvim-tree'.setup {
   },
   filters = {
     dotfiles = false,
-    custom = {}
+    custom = {},
+    exclude = {}
   },
   git = {
     enable = true,
@@ -110,6 +126,7 @@ require'nvim-tree'.setup {
     timeout = 500,
   },
   actions = {
+    use_system_clipboard = true,
     change_dir = {
       enable = true,
       global = false,
@@ -133,6 +150,10 @@ require'nvim-tree'.setup {
     -- if true the tree will resize itself after opening a file
     auto_resize = false,
     hide_root_folder = false,
+    preserve_window_proportions = false,
+    number = false,
+    relativenumber = false,
+    signcolumn = "yes",
     mappings = {
       -- custom only false will merge the list with the default mappings
       -- if true, it will only use your list to set the mappings
@@ -172,13 +193,23 @@ require'nvim-tree'.setup {
         { key = "g?",                                 cb = tree_cb("toggle_help") },
       }
     },
-    number = false,
-    relativenumber = false
   },
   trash = {
     cmd = "trash",
     require_confirm = true
-  }
+  },
+  log = {
+    enable = false,
+    truncate = false,
+    types = {
+      all = false,
+      config = false,
+      copy_paste = false,
+      diagnostics = false,
+      git = false,
+      profile = false,
+    },
+  },
 }
 -------------------------------- Mappings ---------------------------------------
 
