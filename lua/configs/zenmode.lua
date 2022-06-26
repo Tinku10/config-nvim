f0b935348a5e5d01840ccdc6332cc81e95920f56
Tinku Kalita <tinku.kvs@gmail.com>
2022-06-26 22:41:50 +0530

    Bundle similar scoped utilities into common file

--- lua/configs/zenmode.lua
+++ lua/configs/zenmode.lua
@@ -1,4 +1,5 @@
 vim.cmd [[ packadd zen-mode.nvim ]]
+vim.cmd [[ packadd twilight.nvim ]]
 
 require("zen-mode").setup {
   window = {
@@ -47,4 +48,22 @@ require("zen-mode").setup {
   -- callback where you can add custom code when the Zen window closes
   on_close = function()
   end,
+}
+
+require('twilight').setup {
+  dimming = {
+    alpha = 0.25, -- amount of dimming
+    -- we try to get the foreground from the highlight groups or fallback color
+    color = { "Normal", "#ffffff" },
+  },
+  context = 10, -- amount of lines we will try to show around the current line
+  -- treesitter is used to automatically expand the visible text,
+  -- but you can further control the types of nodes that should always be fully expanded
+  expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+    "function",
+    "method",
+    "table",
+    "if_statement",
+  },
+  exclude = {}, -- exclude these filetypes
 }
