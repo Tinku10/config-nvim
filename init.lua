bfab6bb222cc29ea400b6efff57d81d93356ee77
Tinku Kalita <tinku.kvs@gmail.com>
2024-05-12 21:22:36 +0530

    Change default keymap handling

--- init.lua
+++ init.lua
@@ -2,9 +2,9 @@
 require("flags")
 
 -- Load default keymaps
--- Lazy keymaps will be setup by the plugin
--- Event keymaps will be setup by the autocmds
-require("keymaps").create_default_keymaps()
+-- Lazyloaded keymaps will be setup by the plugins
+-- Event-based keymaps will be setup by the autocmds
+require("keymaps")
 
 -- Install and configure plugins
 require("plugins")
